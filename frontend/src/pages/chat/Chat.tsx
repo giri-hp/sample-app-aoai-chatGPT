1e09ed6e6b47a69b83e3d6ce65eb4541071c06f0
AUTH\busam <giridhar.busam@hp.com>
2023-11-13 21:56:05 -0600

    initial changes

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -579,8 +579,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Chat PH2</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Ask questions related to PH2 Assessments. Sure Admin, Batery Safety Mode, MPM Unlock, Sure Recover and Tamper Lock.  </h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
